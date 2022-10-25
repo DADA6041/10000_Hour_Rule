3c9f581c3656341b87af7add60cf2c69d64a911d
DADAKIM <d.ya96v@gmail.com>
2022-10-25 20:19:45 +0900

    edit css and prepare modal

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,8 @@
 // js file
+
+const resultBtn = document.querySelector('.btn-how');
+const result = document.querySelector('#result');
+const modal = document.querySelector('.cheer-modal-wrap');
+const openModal = document.querySelector('.btn-go');
+const closeModal = document.querySelector('#btnClosed');
+
