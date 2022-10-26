df4553423644f384393ec5ce82db544766caec97
DADAKIM <d.ya96v@gmail.com>
2022-10-26 09:16:16 +0900

    create all modal

--- js/10000hours.js
+++ js/10000hours.js
@@ -3,9 +3,9 @@
 const resultBtn = document.querySelector('.btn-how');
 const result = document.querySelector('.result-wrap');
 const resultWarp = document.querySelector('.cont-result');
-const modal = document.querySelector('.cheer-modal-wrap');
-const openModal = document.querySelector('.btn-go');
-const closeModal = document.querySelector('#btnClosed');
+const modal = document.querySelector('#cont-modal');
+const openBtn = document.querySelector('.btn-go');
+const closeBtn = document.querySelector('#btnClosed');
 const shareBtn = document.querySelector('.btn-share');
 const loading = document.querySelector('.img-wrap');
 
@@ -24,8 +24,10 @@ function calc() {
     } else if(timeVal.value == ''){
         alert('입력해주세요!');
         timeVal.focus();
-    } else if(timeVal_int > 24 && timeVal_int < 0) {
+        return false;
+    } else if(timeVal_int > 24 || timeVal_int <= 0) {
         alert('1 ~ 24 사이의 값을 입력해주세요');
+        return false;
     }
     resultWarp.style.display = "block";
     result.style.display = "none";
@@ -34,11 +36,35 @@ function calc() {
     function loadTime(){
         result.style.display = "block";
         loading.style.display = "none";
+        inpResult.innerText = inpVal.value;
+        timeResult.innerText = parseInt((10000/timeVal_int), 10);
     }
 
-    setTimeout (loadTime, 2100);
+    setTimeout (loadTime, 1700);
 }
 
+function openModal() {
+    modal.style.display = "flex"
+}
 
+function closeModal() {
+    modal.style.display = "none";
+}
 
-resultBtn.addEventListener("click", calc);
+function shareUrl(){
+    let url    = window.location.href;
+    let temp   = document.createElement("input");
+    
+    document.body.appendChild(temp);
+    temp.value = url;
+    temp.select();
+    document.execCommand("copy");
+    document.body.removeChild(temp);
+    
+    alert('복사되었습니다!');
+}
+
+resultBtn.addEventListener('click', calc);
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+shareBtn.addEventListener('click', shareUrl);
